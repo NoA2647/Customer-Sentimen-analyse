221626f1f5bbccb4d61df459269aa121274de1a0
amir hossein joghataei <amir.h.joghataei003@gmail.com>
2021-01-24 13:04:38 +0000

    add abs() for negative data

--- chart.py
+++ chart.py
@@ -10,15 +10,15 @@ def chart(reviews, status):
     if status == -1:
         title = 'Negative Aspects'
         output_file("negative_aspects.html")
+        reviews = dict(sorted(reviews.items(), key=lambda item: item[1]))
     if status == 1:
         title = 'Positive Aspects'
         output_file("positive_aspects.html")
-
-    reviews = dict(sorted(reviews.items(), key=lambda item: item[1], reverse=True))
+        reviews = dict(sorted(reviews.items(), key=lambda item: item[1], reverse=True))
 
     for review in reviews:
         aspects.append(review)
-        frequencies.append(reviews[review])
+        frequencies.append(abs(reviews[review]))
 
     p = figure(x_range=aspects, plot_height=500, plot_width=len(aspects) * 100,
                title=title, toolbar_location=None, tools="")
