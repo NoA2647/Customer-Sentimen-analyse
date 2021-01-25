2cca0788a1bc689b03e80d065e6a517ec58883cf
amir hossein joghataei <amir.h.joghataei003@gmail.com>
2021-01-25 18:48:25 +0000

    Replace main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from nltk.stem import PorterStemmer
 import pandas as pd
 import spacy
 import json
+from sentiment import sentiment_review
 from chart import chart
 from radar import beautiful_spider
-from sentiment import sentiment
+import time
 
 
 def find_aspects(nouns):
@@ -26,45 +27,52 @@ def find_aspects(nouns):
     return new_nouns
 
 
+def update_review(doc, key, value):
+    if key in doc.keys():
+        doc[key] = doc[key] + value
+    else:
+        doc[key] = value
+    return doc
+
+
 def main():
-    df = pd.read_csv("C:/Users/Mehrkish/Desktop/Sentiment Analysis/Dataset/phone_user_review_file_1.csv")
+    start = time.time()
+    data = pd.read_csv('DataSet/phone_user_review_file_1.csv', sep=",", encoding='Latin-1')
+    en_data = data[data['lang'] == 'en'].dropna()  # remove other language
+    df = en_data[en_data["phone_url"].str.contains('s5', case=False)].reset_index(drop=True)  # get model data
     nlp = spacy.load("en_core_web_sm")
+
     negative_review = dict()
     positive_review = dict()
     total_rate = 0
+    print("size of model:", len(df['extract']))
 
-    for i in range(1000):
-        print(i)
-        lang = df['lang'][i]
-        if lang == 'en':
-            total_rate += 1
-        else:
-            continue
+    for i in range(len(df['extract'])):
+        print("Pointer =", i+1)
         doc = nlp(df['extract'][i])
-        nouns = sentiment(doc)
-        print(nouns)
+        nouns = sentiment_review(doc)
         aspects = find_aspects(nouns)
         for aspect in aspects:
             total_rate += abs(aspects[aspect])
             if aspects[aspect] < 0:
-                if aspect in negative_review.keys():
-                    negative_review[aspect] = negative_review[aspect] + aspects[aspect]
-                else:
-                    negative_review[aspect] = aspects[aspect]
-
+                negative_review = update_review(negative_review, aspect, aspects[aspect])
             else:
-                if aspect in positive_review.keys():
-                    positive_review[aspect] = positive_review[aspect] + aspects[aspect]
-                else:
-                    positive_review[aspect] = aspects[aspect]
+                positive_review = update_review(positive_review, aspect, aspects[aspect])
 
-    print(total_rate)
+    print("total_rate:", total_rate)
+    end = time.time()
+    elapsed = end - start
+    print("time:", elapsed)
+    '''
     for aspect in positive_review:
         print(aspect, ": {0:.2f}%".format(positive_review[aspect] * 100 / total_rate))
     print("************************************************************")
     for aspect in negative_review:
         print(aspect, ": {0:.2f}%".format(abs(negative_review[aspect]) * 100 / total_rate))
-
+    '''
+    chart(positive_review, total_rate, 1)
+    chart(negative_review, total_rate, -1)
     beautiful_spider(positive_review, negative_review)
+
 
 main()
